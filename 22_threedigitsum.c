ae34d4b59f596a9d983275de7eff5e6949b0f337
Ayushmancodes-08 <patraayushman21@gmail.com>
2024-10-10 17:31:54 +0530

    First Commit

--- 22_threedigitsum.c
+++ 22_threedigitsum.c
@@ -5,7 +5,7 @@ int main()
   printf("Enter a three digit number\n");
   scanf("%d", &num);
   a = num / 100;
-  b = (num % 100) / 10;
+  b = (num % 100) / 10; 
   c = (num % 100) % 10;
   printf("Sum of %d is %d+%d+%d=%d", num, a, b, c, (a + b + c));
 
