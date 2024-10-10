9b51a6f6cec8905b3fbdceb99f15abaa274a2cbc
Ayushmancodes-08 <patraayushman21@gmail.com>
2024-10-10 17:28:57 +0530

    Add files via upload

---
+++ 24_addres.c
@@ -0,0 +1,12 @@
+// WAP to accept a variable and display its adress
+#include <stdio.h>
+int main()
+{
+    int i, *j;
+    printf("Enter value of i");
+    scanf("%d", &i);
+    j = &i;
+    printf("value of i = %d", i);
+    printf("address of i = %u", j);
+    return 0;
+}
