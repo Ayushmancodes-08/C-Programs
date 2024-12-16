21238eb9363a13ab04b46df59f3095ff3f8f9057
Ayushmancodes-08 <patraayushman21@gmail.com>
2024-12-16 13:39:34 +0530

    First commit

--- 83_insertelearra.c
+++ 83_insertelearra.c
@@ -8,7 +8,6 @@ int main()
     int n, ele, pos;
     printf("Enter the size of the array: ");
     scanf("%d", &n);
-
     int arr[n + 1];
     printf("Enter %d elements in the array:\n", n);
     for (int i = 0; i < n; i++)
@@ -33,7 +32,7 @@ int main()
     }
     arr[pos - 1] = ele;
     printf("Array after insertion: ");
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
--- 85_fibofunc.c
+++ 85_fibofunc.c
@@ -1,11 +1,6 @@
 // WAP to print Fibonacci series using Function
 #include <stdio.h>
-int fibonacci(int n) {
-    if (n <= 1)
-        return n;
-    else
-        return (fibonacci(n - 1) + fibonacci(n - 2));
-}
+int fibonacci(int n);
 int main() {
     int n, i;
     printf("Enter the number of terms : ");
@@ -17,4 +12,10 @@ int main() {
     }
     printf("\n");
     return 0;
+}
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return (fibonacci(n - 1) + fibonacci(n - 2));
 }
