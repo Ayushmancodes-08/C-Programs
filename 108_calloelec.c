c3b84236347ebe7ca6fbf7e23ffb2ab2999e3ced
Ayushmancodes-08 <patraayushman21@gmail.com>
2024-12-24 13:18:05 +0530

    First Commit

--- 108_calloelec.c
+++ 108_calloelec.c
@@ -15,12 +15,13 @@ int main() {
     for (int i = 0; i < 5; i++) {
         scanf("%d", &arr[i]);
     }
-    for (int i = 0; i < 5 - 1; i++) {
-        for (int j = i + 1; j < 5; j++) {
-            if (arr[i] > arr[j]) {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+     for (int i = 0; i < 5 - 1; i++)  {
+        for (int j = 0; j < 5 - i - 1; j++){
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
